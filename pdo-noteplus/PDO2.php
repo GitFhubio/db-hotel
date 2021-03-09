4ebafc8964dbe8d27e680bfd5dac7c0098edc854
Fabio Petrone <fa610movic90@gmail.com>
2021-03-09 16:29:31 +0100

    prova pdo2

--- pdo-noteplus/PDO2.php
+++ pdo-noteplus/PDO2.php
@@ -18,35 +18,32 @@ $PDOconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 //inserire record tramite pdo
 //PASSIAMO ISTRUZIONE SQL
 
-$st= $PDOconn->query("INSERT INTO User (nome,username) VALUES ('Luisa','blabla')");
-
-if ($st->rowCount() > 0)
-echo "Record correttamente inserito";
+// $st= $PDOconn->query("INSERT INTO atleti (nome,cognome) VALUES ('Luisa','blabla')");
+//
+// if ($st->rowCount() > 0)
+// echo "Record correttamente inserito";
 
 //c'è lerrore perché la colonna anni non c'è
-try{
-
-  // LA QUERY ritorna un oggetto di tipo statement $st
-$st= $PDOconn->query("INSERT INTO User (nome,username,anni) VALUES ('Luisa','lollo','40')");
-}catch(PDOException $e){
-echo $e->getMessage()";
-}
-
- ?>
+// try{
+//
+//   // LA QUERY ritorna un oggetto di tipo statement $st
+// $st= $PDOconn->query("INSERT INTO User (nome,username,anni) VALUES ('Luisa','lollo','40')");
+// }catch(PDOException $e){
+// echo $e->getMessage()";
+// }
+//
+//
 
  //per visualizzare record nella pagina
 
-
- $st=$PDOconn->query("SELECT * FROM User");
-while($record=$st->fetch()){
-
-  echo "Nome : ". $record['nome'].". Cognome:" .$record['cognome']."<br>";
-}
+ $st=$PDOconn->query("SELECT * FROM atleti");
+// while($record=$st->fetch()){
+//
+//   echo "Nome : ". $record['nome'].". Cognome:" .$record['cognome']."<br>";
+// }
 
 
 //oppure possiamo fare al posto del while
-
-$elenco = $st-> fetchALL();
-var_dump($elenco);
-
+// $elenco = $st->fetchAll();
+// var_dump($elenco);
 //metodo query mette a rischio di sql injection
